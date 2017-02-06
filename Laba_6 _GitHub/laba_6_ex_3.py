10c3b14d0837f5e8821b045931b7ea84d90bcdde
Denis Semenov <denissemo1999@gmail.com>
2017-02-07 00:54:00 +0200

    Laba_6 Commit

--- Laba_6 _GitHub/laba_6_ex_3.py
+++ Laba_6 _GitHub/laba_6_ex_3.py
@@ -3,27 +3,37 @@
 from enum import Enum
 class Continent(Enum):
     Asia = 1
-    America = 2
-    Europe = 3
+    Europe = 2
+    America = 3
 
 class Country(Enum):
-    Germany = 3
+    Germany = 1
     Cuba = 2
-    Laos = 1
-    Monaco = 3
-    Bangladesh = 1
-    Ukraine = 3
+    Laos = 3
+    Monaco = 4
+    Bangladesh = 5
+    Ukraine = 6
+
+Asia = Country.Bangladesh.name, Country.Laos.name
+Europe = Country.Germany.name, Country.Ukraine.name, Country.Monaco.name
+America = Country.Cuba.name
 while True:
     try:
         user_inp = input('\nВведите название страны на англ. языке: ')
         user_inp = user_inp.capitalize()
-        s = Country[user_inp]
-        s = s.value
+        s = Country[user_inp].name
     except KeyError or ValueError:
         print('Такой страны нет в списке, может вы ошиблись, давайте попробуем'
         ' снова:)')
         continue
-    print('Континент: ', Continent(s).name)
+    for i in Asia:
+        if i == s:
+            print('Континент: ', Continent.Asia.name)
+    for i in Europe:
+        if i == s:
+            print('Континент: ', Continent.Europe.name)
+    if America == s:
+        print('Континент: ', Continent.America.name)
     ask = input('Хотите продолжить? [y/n]: ')
     if ask == 'y':
         continue
