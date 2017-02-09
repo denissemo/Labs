8f5dda575333d64df0611b06b4d49aed24014483
Denis Semenov <denissemo1999@gmail.com>
2017-02-09 19:06:38 +0200

    Laba_6 Commit

--- Laba_6 _GitHub/laba_6_ex_1.py
+++ Laba_6 _GitHub/laba_6_ex_1.py
@@ -1,14 +1,5 @@
 # Семенов Денис КНІТ 16-А
 # Условие: по заданой дате, определить дату следующего дня
-def next_month(m):
-    """Функция меняет месяц на следующий,\
-     если введен 31 день месяца"""
-    if m in range(12):
-        m += 1
-    else:
-        m = 1
-    return m
-
 
 while True:
     while True:
@@ -44,8 +35,11 @@ while True:
     day += 1
     if day not in range(32):
         day = 1
-        month = next_month(month)
-        year += 1
+        if month in range(12):
+            month += 1
+        else:
+            month = 1
+            year += 1
     print('Дата следующего дня: {}.{}.{}'.format(day, month, year))
     ask = input('Хотите продолжить? [y/n]: ')
     if ask == 'y':
