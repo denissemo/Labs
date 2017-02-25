9c9f8fd05e8a8d8e58b3f29196d02130813f0805
Denis Semenov <denissemo1999@gmail.com>
2017-02-25 17:55:39 +0200

    Second commit

--- Laba_7_GitHub/laba_7_individual_(b).py
+++ Laba_7_GitHub/laba_7_individual_(b).py
@@ -6,45 +6,47 @@
 
 import re
 let_case = {'б', 'в', 'г', 'д', 'ж', 'з', 'й', 'л', 'м', 'н', 'р'}
-letters = []
+inp_let = set()  # Множество согласных звонких букв в текущей строке
+prev_let = set()  # Множество согласных звонких букв, обнаруженных в предыдущих словах
+out_let = set()  # Множество согласных, которые присутствуют более, чем в одном слове
 while True:
     print('Enter the words, put comma between them and at the end point:')
     w_inp = input().lower()
     if w_inp.isdigit():
-        print('Please input string!')
+        print('Please input string!\n')
         continue
     else:
         if re.search('[а-я]', w_inp):
             if w_inp.endswith('.'):
-                if ',' in w_inp or (',' not in w_inp and w_inp.endswith('.')
-                                    and ' ' not in w_inp):
+                if ',' in w_inp:
                     w_inp = w_inp.split(', ')
                     for i in w_inp:
                         for j in i:
                             if j in let_case:
-                                letters.append(j)
-                    for c in letters:
-                        if letters.count(c) == 2:
-                            pass
-                        else:
-                            letters.remove(c)
-                            print('None available letters')
+                                inp_let.add(j)
+                                out_let |= inp_let & prev_let
+                                prev_let |= inp_let
+                                inp_let.clear()
+                    if out_let == set():
+                        print('None available letters! Try again.\n')
+                        continue
+                    else:
+                        w_out = sorted(out_let)
+                        w_str = '; '.join(w_out)
+                        print('Voiced consonants letters: ', w_str + '.')
                 else:
-                    print('You have to put commas between words!')
+                    print('You have to put commas between words, or you wri'
+                    'te one word!\n')
                     continue
             else:
-                print('You have to put point after words!')
+                print('You have to put point after words!\n')
                 continue
-
         else:
-            print('Word should be written in the Russian alphabet!')
+            print('Word should be written in the Russian alphabet!\n')
             continue
-    letters = set(letters)
-    w = sorted(letters)
-    w_str = '; '.join(w)
-    print('Voiced consonants letters: ', w_str)
-    ask = input('Do you want continue? [y/n]: ')
+    ask = input('\nDo you want continue? [y/n]: ')
     if ask == 'y':
+        out_let.clear()
         continue
     else:
         break
