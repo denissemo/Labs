5d04b4d999beb52cf771d5dca127f31485f4fa88
Denis Semenov <denissemo1999@gmail.com>
2017-03-03 22:21:58 +0200

    First commit

--- Laba_8_GItHub/laba_8_11(a).py
+++ Laba_8_GItHub/laba_8_11(a).py
@@ -6,13 +6,13 @@ while True:
     try:
         while True:
             n = int(input('Input tapes: '))
-            if n < 1:
+            if n < 2:
                 print('input correct number\n')
                 continue
             break
         while True:
             m = int(input('Input columns: '))
-            if m < 1:
+            if m < 2:
                 print('input correct number\n')
                 continue
             break
@@ -21,14 +21,12 @@ while True:
         continue
     if n == m:
         array = np.zeros((n, m))
-        i_1 = 0
         flag = False
-        for i in range(n-1, -1, -1):
-            i_1 += 1
+        for i in range(n):
             for j in range(m):
                 try:
                     array[i][j] = float(input('Input element of matrix %d%d: ' %
-                                    (i_1, j + 1)))
+                                              (i + 1, j + 1)))
                 except ValueError:
                     print('Please enter the number, you enter words!\n')
                     flag = True
@@ -36,12 +34,13 @@ while True:
             if flag:
                 break
         if not flag:
-            array = np.swapaxes(array, 0, 1)
-            print('Array turn to 90 degrees:\n', array)
+            print('\nSource array:\n', array)
+            array = np.rot90(array, 3)
+            print('\nArray turn to 90 degrees:\n', array)
     else:
         print('Array must be a square!\n')
         continue
-    ask = input('Do you want continue? [y/n]: ')
+    ask = input('\nDo you want continue? [y/n]: ')
     if ask == 'y':
         print()
         continue
