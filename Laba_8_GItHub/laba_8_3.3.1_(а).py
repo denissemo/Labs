3770f5580c5d3820e428b66a7ebe5a73c80c17b2
Denis Semenov <denissemo1999@gmail.com>
2017-03-17 00:07:10 +0200

    add time measurement

--- Laba_8_GItHub/laba_8_3.3.1_(а).py
+++ Laba_8_GItHub/laba_8_3.3.1_(а).py
@@ -1,9 +1,11 @@
 # Семенов Денис КНІТ 16-А
 """Условие: линейный поиск"""
 
+from timeit import timeit
+setup = '''
 import numpy as np
 from random import choice, randint
-from string import ascii_uppercase
+from string import ascii_letters
 
 def linear_search(a, x):
     a = np.append(a, [x])
@@ -12,23 +14,22 @@ def linear_search(a, x):
         i += 1
     return i if i < len(a) - 1 else None
 
-while True:
-    letters = ascii_uppercase
-    array = np.zeros(randint(10, 15), dtype = str)
-    for j in range(len(array)):
-        array[j] = choice(letters)
 
-    print('Source array:\n', array)
-    c_elem = choice(letters)
-    print('\nRandomly selected item from sequence:\n == {} =='.format(c_elem))
-    returned = linear_search(array, c_elem)
-    if returned is None:
-        print('\nElement not found...')
-    else:
-        print('\nElement {} was found at {} position'.format(c_elem, returned))
+letters = ascii_letters
+array = np.zeros(108, dtype = str)
+for j in range(len(array)):
+    array[j] = choice(letters)
 
-    ask = input('\nDo you want try again? [y/n]: ')
-    if ask == 'y':
-        continue
-    else:
-        break
+print('Source array:\\n', array)
+c_elem = choice(letters)
+print('\\nRandomly selected item from sequence:\\n == {} =='.format(c_elem))
+returned = linear_search(array, c_elem)
+if returned is None:
+    print('\\nElement not found...')
+else:
+    print('\\nElement |{}| was found at {} position'.format(c_elem, returned))
+print('\\nThe amount of memory, consumed by the linear search algorithm {} bytes'
+      .format(returned.__sizeof__()))
+'''
+time = timeit(setup, number = 1)
+print('\nTime of algorithm execution {:1.3f} second'.format(time))
