937935e6586568e35e7c3ef5c3b1b71a207e1061
Denis Semenov <denissemo1999@gmail.com>
2017-03-18 23:35:25 +0200

    first commit

---
+++ Laba_8_GItHub/laba_8_3.3.1_(b).py
@@ -0,0 +1,42 @@
+# Семенов Денис КНІТ 16-А
+"""Условие: бинарный поиск"""
+
+from timeit import timeit
+setup = '''
+import numpy as np
+from random import randint
+
+def binary_search(a, x):
+    s = 0
+    e = len(a) - 1
+    answer = None
+    while s <= e:
+        mid = (s + e) // 2
+        if a[mid] == x:
+            answer = mid
+            break
+        elif a[mid] > x:
+            e = mid - 1
+        elif a[mid] < x:
+            s = mid + 1
+    return answer
+
+array = np.zeros(108, dtype = int)
+for i in range(len(array)):
+    array[i] = randint(0, 200)
+
+print('Source array:\\n', array)
+s_array = np.sort(array)
+print('\\nSorted array:\\n', s_array)
+c_elem = randint(0, 200)
+print('\\nRandomly selected item from sequence:\\n == {} =='.format(c_elem))
+returned = binary_search(s_array, c_elem)
+if returned is None:
+    print('\\nElement not found...')
+else:
+    print('\\nElement |{}| was found at {} position'.format(c_elem, returned))
+print('\\nThe amount of memory, consumed by the linear search algorithm {} bytes'
+      .format(returned.__sizeof__()))
+'''
+time = timeit(setup, number = 1)
+print('\nTime of algorithm execution {:1.3f} second'.format(time))
