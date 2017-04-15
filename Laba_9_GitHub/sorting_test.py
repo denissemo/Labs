e0c17bdbc2b30a7250fcdf3bd66a477bca6a1555
Denis Semenov <denissemo1999@gmail.com>
2017-04-15 23:34:06 +0300

    improved, not all

--- Laba_9_GitHub/sorting_test.py
+++ Laba_9_GitHub/sorting_test.py
@@ -1,65 +1,110 @@
 # Семенов Денис КНІТ 16-А
 """программа для теста алгоритмов"""
 
+import numpy as np
 from time import time
 
+
 def bubble_sort(n, a):
+    """Пузырьковая сортировка.
+
+    Функция принимает два параметра: длину массива(n), и сам массив(а)
+    """
     flag = True
+    c1 = 0
+    c2 = 0
     for i in range(1, n):
         if flag:
             flag = False
             for j in range(n - 1, i - 1, -1):
+                c1 += 1
                 if a[j - 1] > a[j]:
-                    min = a[j]
-                    a[j], a[j - 1] = a[j - 1], min
+                    a[j], a[j - 1] = a[j - 1], a[j]
+                    c2 += 1
                     flag = True
         else:
             break
+    return c1, c2
 
 
 def selection_sort(n, a):
+    """Сортировка выбором.
+
+    Функция принимает два параметра: длину массива(n), и сам массив(а)
+    """
+    c1 = 0
+    c2 = 0
     for i in range(n):
         min = i
         for j in range(i + 1, n):
+            c1 += 1
             if a[j] < a[min]:
                 min = j
+        c2 += 1
         a[i], a[min] = a[min], a[i]
+    return c1, c2
 
 
 def insertion_sort(n, a):
+    """Сортировка вставками.
+
+    Функция принимает два параметра: длину массива(n), и сам массив(а)
+    """
+    c1 = 0
+    c2 = 0
     for i in range(n):
         j = i
+        c1 += 1
         while j > 0 and a[j] < a[j - 1]:
             a[j], a[j - 1] = a[j - 1], a[j]
+            c2 += 1
             j -= 1
+    return c1, c2
 
 
 def cocktail_sort(n, a):
+    """Сортировка перемешиванием.
+
+    Функция принимает два параметра: длину массива(n), и сам массив(а)
+    """
+    c1 = 0
+    c2 = 0
     for k in range(n - 1, 0, -1):
         flag = False
         for i in range(k, 0, -1):
+            c1 += 1
             if a[i] < a[i - 1]:
                 a[i], a[i - 1] = a[i - 1], a[i]
+                c2 += 1
                 flag = True
         for i in range(k):
+            c1 += 1
             if a[i] > a[i + 1]:
                 a[i], a[i + 1] = a[i + 1], a[i]
+                c2 += 1
                 flag = True
         if not flag:
-            return a
+            return c1, c2
 
 
 def shell_Sort(n, a):
-    d = n // 2
+    """Сортировка Шелла.
+
+    Функция принимает два параметра: длину массива(n), и сам массив(а)
+    """
+    c1 = 0
+    c2 = 0
+    d = n // 2  # делит последовательность пополам
     while d > 0:
-        for i in range(d, n):
-            tmp = a[i]
+        for i in range(d, n, d):
             j = i
-            while j > 0 and tmp < a[j - d]:
-                a[j] = a[j - d]
+            c1 += 1
+            while j > 0 and a[j] < a[j - d]:
+                a[j], a[j - d] = a[j - d], a[j]
+                c2 += 1
                 j -= d
-            a[j] = tmp
         d //= 2
+    return c1, c2
 
 
 def swap(i, j, a):
@@ -88,9 +133,12 @@ def heap_sort(n):
 
 
 while True:
-    file = open('py_alg.txt')
-    a = file.read()
-    array = a.split(',')
+    arr_file = open('test_array.txt')
+    a = arr_file.read()
+    array = np.zeros(10000, dtype = int)  # Тестовый массив 10000 случайных целых чисел
+    arr = a.split(',')
+    for i in range(len(arr)):
+        array[i] = arr[i]
     print('Source array: ', array, '\n')
     print('Which sort method you want to use?\n', '1 - Bubble sort\n', '2'
         ' - Selection sort\n', '3 - Insertion sort\n', '4 - Cocktail sort\n'
@@ -108,43 +156,74 @@ while True:
             break
     if sort_m == 1:
         s_t = time()
-        bubble_sort(10000, array)
+        returned = bubble_sort(10000, array)
         print('\nSorted array: ', array)
+        print('\nNumber of comparisons: {}\nNumber of exchanges: {}'
+              .format(returned[0], returned[1]))
         p_t = time() - s_t
         print('\nTime of algorithm execution {:f} second'.format(p_t))
+        data_file = open('test_data.txt', 'a')
+        data_file.write('\nBubble sort:\n time: {}\n comparisons: {}\n exchange'
+                        's: {}\n'.format(str(p_t), returned[0], returned[1]))
+        data_file.close()
     elif sort_m == 2:
         s_t = time()
-        selection_sort(10000, array)
+        returned = selection_sort(10000, array)
         print('\nSorted array: ', array)
+        print('\nNumber of comparisons: {}\nNumber of exchanges: {}'
+              .format(returned[0], returned[1]))
         p_t = time() - s_t
         print('\nTime of algorithm execution {:f} second'.format(p_t))
+        data_file = open('test_data.txt', 'a')
+        data_file.write('\nSelection sort:\n time: {}\n comparisons: {}\n excha'
+                        'nges: {}\n'.format(str(p_t), returned[0], returned[1]))
+        data_file.close()
     elif sort_m == 3:
         s_t = time()
-        insertion_sort(10000, array)
+        returned = insertion_sort(10000, array)
         print('\nSorted array: ', array)
+        print('\nNumber of comparisons: {}\nNumber of exchanges: {}'
+              .format(returned[0], returned[1]))
         p_t = time() - s_t
         print('\nTime of algorithm execution {:f} second'.format(p_t))
+        data_file = open('test_data.txt', 'a')
+        data_file.write('\nInsertion sort:\n time: {}\n comparisons: {}\n excha'
+                        'nges: {}\n'.format(str(p_t), returned[0], returned[1]))
+        data_file.close()
     elif sort_m == 4:
         s_t = time()
-        cocktail_sort(10000, array)
+        returned = cocktail_sort(10000, array)
         print('\nSorted array: ', array)
+        print('\nNumber of comparisons: {}\nNumber of exchanges: {}'
+              .format(returned[0], returned[1]))
         p_t = time() - s_t
         print('\nTime of algorithm execution {:f} second'.format(p_t))
+        data_file = open('test_data.txt', 'a')
+        data_file.write('\nCocktail sort:\n time: {}\n comparisons: {}\n exchan'
+                        'ges: {}\n'.format(str(p_t), returned[0], returned[1]))
+        data_file.close()
     elif sort_m == 5:
         s_t = time()
-        shell_Sort(10000, array)
+        returned = shell_Sort(10000, array)
         print('\nSorted array: ', array)
+        print('\nNumber of comparisons: {}\nNumber of exchanges: {}'
+              .format(returned[0], returned[1]))
         p_t = time() - s_t
         print('\nTime of algorithm execution {:f} second'.format(p_t))
+        data_file = open('test_data.txt', 'a')
+        data_file.write('\nShell sort:\n time: {}\n comparisons: {}\n exchanges'
+                        ': {}\n'.format(str(p_t), returned[0], returned[1]))
+        data_file.close()
     elif sort_m == 6:
         s_t = time()
         heap_sort(10000)
         print('\nSorted array: ', array)
         p_t = time() - s_t
         print('\nTime of algorithm execution {:f} second'.format(p_t))
-    ask = input('\nDo you want to try sort array again? [y/n]: ').lower()
-    if ask == 'y':
+    ask = input('\nDo you want to try sort array again? [1/0]: ')
+    if ask == '1':
         print()
         continue
     else:
+        arr_file.close()
         break
